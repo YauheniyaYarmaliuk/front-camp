598f1f6757b40c785839136a970683640514504a
Yauheniya Yarmaliuk <yauheniya_yarmaliuk@epam.com>
2016-11-21 15:54:19 +0300

    refact html

--- js/hackerNews.js
+++ js/hackerNews.js
@@ -1,5 +1,4 @@
-    const result = fetch('https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=5c16623f895c44948812458d23c5396e')
-
+  const result = fetch('https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=5c16623f895c44948812458d23c5396e')
     result.then((response) => response.json()
     ).then((json) => generateNews(json)
     ).catch((ex) => console.error('failed', ex));
