95bad4101e978f30f234009774ae2f0c579751cd
Yauheniya Yarmaliuk <yauheniya_yarmaliuk@epam.com>
2017-02-13 18:34:07 +0300

    refactor

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ var routes = require('./server/routes/index'),
 //var users = require('./routes/users');
 
 var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8090");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.static(path.join(__dirname, 'public')));
